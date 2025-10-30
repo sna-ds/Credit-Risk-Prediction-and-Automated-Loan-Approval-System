ef7f68667f814e156fc811b6d0ce97678497e61d
Suciningtyas Nur Alifah <suciningtyas.n.a@gmail.com>
2025-10-30 12:10:49 +0700

    change on app.py

--- app.py
+++ app.py
@@ -4,36 +4,40 @@ from streamlit_navigation_bar import st_navbar
 import pages as pg
 
 # Page config
-st.set_page_config(page_title="Loan Risk Prediction", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(
+    page_title="Loan Risk Prediction",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
 
 # Navigation setup
 pages = ["Home", "Analysis", "Risk Scoring", "Loan Approval"]
+
+# Path for logo
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 logo_path = os.path.join(parent_dir, "images", "icons8-home.svg")
 
 styles = {
     "nav": {
-        "background-color": "#0d6efd",
+        "background-color": "royalblue",
         "justify-content": "left",
-        "padding": "8px",
     },
     "img": {
-        "padding-right": "10px",
+        "padding-right": "14px",
     },
     "span": {
         "color": "white",
-        "padding": "10px",
-        "font-weight": "500",
+        "padding": "14px",
     },
     "active": {
         "background-color": "white",
-        "color": "#1559be",
-        "font-weight": "600",
-        "padding": "10px",
-        "border-radius": "8px",
-    }
+        "color": "var(--text-color)",
+        "font-weight": "normal",
+        "padding": "14px",
+    },
 }
 
+# Navbar setup
 options = {
     "show_menu": False,
     "show_sidebar": False,
@@ -46,12 +50,12 @@ page = st_navbar(
     options=options,
 )
 
-# Page routing
+# Routing
 if page == "Home":
     pg.home.show_home()
 elif page == "Analysis":
-    pg.overview.show_overview()
+    pg.analysis.show_analysis()
 elif page == "Risk Scoring":
-    pg.risk_scoring.show_risk_scoring()
+    pg.risk_score.show_risk_scoring()
 elif page == "Loan Approval":
     pg.loan_approval.show_loan_approval()
