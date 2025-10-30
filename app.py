294d21b80d8b77bc3a3605a4595dc1827366b241
Suciningtyas Nur Alifah <suciningtyas.n.a@gmail.com>
2025-10-30 09:20:41 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+import streamlit as st
+from streamlit_navigation_bar import st_navbar
+
+import pages as pg
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+pages = ["Analysis", "Risk Scoring", "Loan Approval"]
