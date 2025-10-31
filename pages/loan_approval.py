916ed0938b64ac328660026ba08b414df209098b
Suciningtyas Nur Alifah <suciningtyas.n.a@gmail.com>
2025-10-31 16:26:11 +0000

    update loan_approval.py

--- pages/loan_approval.py
+++ pages/loan_approval.py
@@ -1,5 +1,26 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+import shap
 
 def show_loan_approval():
-    st.title("Automated Loan Approval Decision")
-    st.write("This module automates the loan approval decision process using a binary classification model. It leverages the previously predicted Risk Score and key financial features to determine whether an applicant should be approved or denied.")
+    # --- Load model dan preprocessing ---
+    model = joblib.load("models/lgbm_classifier.pkl")
+    scaler = joblib.load("models/clf_scaler.pkl")
+    encoder = joblib.load("models/encoder.pkl")
+
+    selected_features = [
+        'RiskScore', 'DebtToIncomeRatio', 'BankruptcyHistory', 'CreditScore', 'NetWorth',
+        'MonthlyIncome', 'LoanAmount', 'InterestRate', 'PreviousLoanDefaults', 'CreditCardUtilizationRate'
+    ]
+
+    st.title("Automated Loan Approval System")
+    st.markdown("""
+    Input applicant details below to predict whether the loan will be **approved** or **denied** 
+    based on financial and risk indicators.
+    
+    Choose between:
+    - **Single Applicant** (manual input)
+    - **Batch Upload** (upload CSV file)
+    """)
